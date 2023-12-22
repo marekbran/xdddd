a931084305dfbeb5b2b32c5833507469e232771d
@hermana4 <adam.hermann@aalto.fi>
2023-12-22 23:50:39 +0200

    now ?

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 import { serve } from "./deps.js";
 import { configure, renderFile } from "./deps.js";
 
-
 configure({
   views: `${Deno.cwd()}/views/`,
 });
@@ -14,12 +13,15 @@ const data = {
   count: 0,
 };
 
+const data_2 = {
+  imageSrc: "source.jpg", // Use the actual path to your image
+};
+
 const handleRequest = async (request) => {
   const url = new URL(request.url);
   if (url.pathname === "/" && request.method === "GET") { 
-    return new Response( await renderFile("./layouts/layout.eta"), responseDetails);
-  }
-  else if (url.pathname === "/count") {
+    return new Response(await renderFile("./layouts/layout.eta", data_2), responseDetails);
+  } else if (url.pathname === "/count") {
     data.count++;
     return new Response(await renderFile("count.eta", data), responseDetails);
   }
