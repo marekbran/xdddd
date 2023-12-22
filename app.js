f2a5ae4d835aae0ba66d8895d9741c396a24c5dd
@hermana4 <adam.hermann@aalto.fi>
2023-12-22 23:54:12 +0200

    now ?

--- app.js
+++ app.js
@@ -9,22 +9,17 @@ const responseDetails = {
   headers: { "Content-Type": "text/html;charset=UTF-8" },
 };
 
-const data = {
-  count: 0,
-};
+
 
 const data_2 = {
-  imageSrc: "source.jpg", // Use the actual path to your image
+  imageSrc: "./source.jpg", // Use the actual path to your image
 };
 
 const handleRequest = async (request) => {
   const url = new URL(request.url);
   if (url.pathname === "/" && request.method === "GET") { 
     return new Response(await renderFile("./layouts/layout.eta", data_2), responseDetails);
-  } else if (url.pathname === "/count") {
-    data.count++;
-    return new Response(await renderFile("count.eta", data), responseDetails);
-  }
+  } 
 };
 
 serve(handleRequest, { port: 7777 });
