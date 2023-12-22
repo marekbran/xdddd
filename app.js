5d85cd5cfb365ae38f1b5f5e26b1764577bc4c2d
@hermana4 <adam.hermann@aalto.fi>
2023-12-23 00:00:23 +0200

    now ?

--- app.js
+++ app.js
@@ -1,15 +1,23 @@
-import { serve } from "./deps.js";
+import { Application, send } from "https://deno.land/x/abc/mod.ts";
 import { configure, renderFile } from "./deps.js";
+import { join } from "https://deno.land/std/path/mod.ts";
 
 configure({
   views: `${Deno.cwd()}/views/`,
 });
 
+const app = new Application();
+const staticBasePath = join(Deno.cwd(), "./source.jpg");
+
+app.use(async (ctx) => {
+  await send(ctx, ctx.request.url.pathname, {
+    root: staticBasePath,
+  });
+});
+
 const responseDetails = {
   headers: { "Content-Type": "text/html;charset=UTF-8" },
 };
-
-
 
 const data_2 = {
   imageSrc: "./source.jpg", // Use the actual path to your image
@@ -17,9 +25,9 @@ const data_2 = {
 
 const handleRequest = async (request) => {
   const url = new URL(request.url);
-  if (url.pathname === "/" && request.method === "GET") { 
+  if (url.pathname === "/" && request.method === "GET") {
     return new Response(await renderFile("./layouts/layout.eta", data_2), responseDetails);
-  } 
+  }
 };
 
-serve(handleRequest, { port: 7777 });
+app.start({ port: 7777 });
